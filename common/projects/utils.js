537e409faa5d180e99ed3c6458a2a7f0bcf85af6
ethdumplingz <94034316+ethdumplingz@users.noreply.github.com>
2022-03-02 17:15:53 -0500

    Added 3 new columns to the project row

--- common/projects/utils.js
+++ common/projects/utils.js
@@ -3,7 +3,7 @@ const baseAppLoggingTag = `[PROJECTS]`;
 
 const table = `projects`;
 
-const insertProject = async ({id, title = '', description = '', website_url = '', twitter_url = '', discord_url= '', is_discord_open= true, presale_price = 0, sale_unit = "ETH", ts_presale_start = 0, ts_presale_end = 0, wl_register_url = ""} = {}) => {
+const insertProject = async ({id, title = '', description = '', website_url = '', twitter_url = '', discord_url= '', is_discord_open= true, presale_price = 0, sale_unit = "ETH", ts_presale_start = 0, ts_presale_end = 0, wl_register_url = "", max_supply = 0, max_per_transaction = 0, max_per_wallet = 0} = {}) => {
 	const loggingTag = `${baseAppLoggingTag}[insertProject]`;
 	let outcome;
 	try{
@@ -11,8 +11,8 @@ const insertProject = async ({id, title = '', description = '', website_url = ''
 		// console.info(`${loggingTag} got client`, client);
 		const insertQuery = {
 			name: `add-project`,
-			text: `INSERT INTO ${table}(id, title, description, website_url, twitter_url, discord_url, is_discord_open, presale_price, sale_unit, ts_presale_start, ts_presale_end, wl_register_url) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
-			values: [id, title, description, website_url, twitter_url, discord_url, is_discord_open, presale_price, sale_unit, ts_presale_start, ts_presale_end, wl_register_url]
+			text: `INSERT INTO ${table}(id, title, description, website_url, twitter_url, discord_url, is_discord_open, presale_price, sale_unit, ts_presale_start, ts_presale_end, wl_register_url, max_supply, max_per_transaction, max_per_wallet) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)`,
+			values: [id, title, description, website_url, twitter_url, discord_url, is_discord_open, presale_price, sale_unit, ts_presale_start, ts_presale_end, wl_register_url, max_supply, max_per_transaction, max_per_wallet]
 		};
 		console.info(`${loggingTag} adding project. query`, insertQuery);
 		const result = outcome = await client.query(insertQuery);
@@ -25,15 +25,15 @@ const insertProject = async ({id, title = '', description = '', website_url = ''
 	return outcome;
 }
 
-const updateProject = async ({id, title = '', description = '', website_url = '', twitter_url = '', discord_url= '', is_discord_open= true, presale_price = 0, sale_unit = "ETH", ts_presale_start = 0, ts_presale_end = 0, wl_register_url = ""} = {}) => {
+const updateProject = async ({id, title = '', description = '', website_url = '', twitter_url = '', discord_url= '', is_discord_open= true, presale_price = 0, sale_unit = "ETH", ts_presale_start = 0, ts_presale_end = 0, wl_register_url = "", max_supply = 0, max_per_transaction = 0, max_per_wallet = 0} = {}) => {
 	const loggingTag = `${baseAppLoggingTag}[updateProject]`;
 	let outcome;
 	try{
 		const client = await db.connection.get();
 		// console.info(`${loggingTag} got client`, client);
 		const updateQuery = {
-			text: `UPDATE ${table} SET title = $2, description = $3, website_url = $4, twitter_url = $5, discord_url = $6, is_discord_open = $7, presale_price = $8, sale_unit = $9, ts_presale_start = $10, ts_presale_end = $11, wl_register_url = $12 WHERE id = $1`,
-			values: [id, title, description, website_url, twitter_url, discord_url, is_discord_open, presale_price, sale_unit, ts_presale_start, ts_presale_end, wl_register_url]
+			text: `UPDATE ${table} SET title = $2, description = $3, website_url = $4, twitter_url = $5, discord_url = $6, is_discord_open = $7, presale_price = $8, sale_unit = $9, ts_presale_start = $10, ts_presale_end = $11, wl_register_url = $12, max_supply = $13, max_per_transaction = $14, max_per_wallet = $15 WHERE id = $1`,
+			values: [id, title, description, website_url, twitter_url, discord_url, is_discord_open, presale_price, sale_unit, ts_presale_start, ts_presale_end, wl_register_url, max_supply, max_per_transaction, max_per_wallet]
 		};
 		
 		console.info(`${loggingTag} updating project. query`, updateQuery);
