0c01b9f08a7bd9afd8ec7dc99265c4bb14debdf3
ethdumplingz <94034316+ethdumplingz@users.noreply.github.com>
2022-03-14 01:26:52 -0400

    increase rate limit from 100 per min to 500 per min

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const app = express(),
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-	windowMs: 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	windowMs: 60 * 1000, // 1 minute
+	max: 500, // Limit each IP to 500 requests per `window` (here, per 1 minute)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
